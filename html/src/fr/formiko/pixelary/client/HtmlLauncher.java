971217f93b4aeb6f03ec5f44b162c2fffaa9a239
HydrolienF <hydrolien.f@gmail.com>
2023-03-19 14:22:48 +0100

    It compile

--- html/src/fr/formiko/pixelary/client/HtmlLauncher.java
+++ html/src/fr/formiko/pixelary/client/HtmlLauncher.java
@@ -1,22 +1,25 @@
 package fr.formiko.pixelary.client;
 
+import fr.formiko.pixelary.Pixelary;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import fr.formiko.pixelary.Pixelary;
+import com.badlogic.gdx.backends.gwt.GwtGraphics;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+        GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+        // Fixed size application:
+        // return new GwtApplicationConfiguration(1600, 900);
+        cfg.fullscreenOrientation = GwtGraphics.OrientationLockType.LANDSCAPE;
+        cfg.padHorizontal = 0;
+        cfg.padVertical = 0;
+        return cfg;
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new Pixelary();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() { return new Pixelary(); }
 }
